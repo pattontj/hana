f56c24e9347ad26b0141e8a41bb23d916b69c797
pattontj <pattontj@users.noreply.github.com>
2025-01-28 00:26:36 -0800

    added a grammar and the beginnings of some parsing functions

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,63 @@
+use std::env;
+use std::fs;
 
-use fs;
+use self::AstNode::*;
 
+use pest::error::Error;
 use pest::Parser;
 use pest_derive::Parser;
-
 
 #[derive(Parser)]
 #[grammar = "hana.pest"]
 pub struct HanaParser;
 
+pub enum AstNode {
+    Print(Box<AstNode>),
+    Integer(i32),
+    DoublePrecisionFloat(f64),
+    Sexpr {
+        lhs: Box<AstNode>,
+        rhs: Box<AstNode>,
+    },
+}
+
+pub fn parse(source: &str) -> Result<Vec<AstNode>, Error<Rule>> {
+    let mut ast = vec![];
+
+    let pairs = HanaParser::parse(Rule::program, source)?;
+
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::sexpr => {
+                ast.push(Print(Box::new(build_ast_from_sexpr(pair))));
+            }
+            _ => {}
+        }
+    }
+
+    Ok(ast)
+}
+
+fn build_ast_from_sexpr(pair: pest::iterators::Pair<Rule>) -> AstNode {
+    match pair.as_rule() {
+        Rule::integer => {}
+        Rule::real => {}
+        Rule::string => {}
+        Rule::ident => {}
+        Rule::sexpr => {}
+        _ => {}
+    }
+    Integer(0)
+}
+
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
+    let unparsed_file = fs::read_to_string("src/test.hana").expect("cannot read file!");
 
-    let unparsed_file = fs::read_to_string("src/test.hana");
+    let file = HanaParser::parse(Rule::program, &unparsed_file)
+        .expect("unsuccessful parse")
+        .next()
+        .unwrap();
 
-    let file = HanaParser::parse(Rule::file, &unparsed_file)
-        .next().unwrap();
-   
     println!("{:?}", file);
 }
